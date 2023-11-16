c4dabec148f2b44393ba054b0fab0b8009c1144b
TaozheLi <Taozhe.Li-1@ou.edu>
2023-11-16 09:54:05 -0600

    modify hello.cu

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -2,7 +2,7 @@
 
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x;
+  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
