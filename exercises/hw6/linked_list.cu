60023d222e183d6c99c808349443dd2c7cc8d1d6
TaozheLi <Taozhe.Li-1@ou.edu>
2023-11-19 20:47:55 -0600

    HW6

--- exercises/hw6/linked_list.cu
+++ exercises/hw6/linked_list.cu
@@ -21,7 +21,9 @@ struct list_elem {
 template <typename T>
 void alloc_bytes(T &ptr, size_t num_bytes){
 
-  ptr = (T)malloc(num_bytes);
+  // ptr = (T)malloc(num_bytes);
+  // UM version
+  cudaMallocManaged(&ptr, num_bytes);
 }
 
 __host__ __device__
